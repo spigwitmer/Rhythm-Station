6c147d0786798ecff39437e1e4f77a1e232a698a
Colby Klein <shakesoda@gmail.com>
2010-11-02 01:16:19 -0700

    Specify <float, float, float> constructor for vec3 and add access to x, y, z (which doesn't seem to work)

--- src/Type.cpp
+++ src/Type.cpp
@@ -28,12 +28,15 @@ std::string toString(bool _in)
 #include <SLB/SLB.hpp>
 void Type_Binding() {
 	SLB::Class<vec3>("vec3")
-        .constructor()
-        .set("normalize", &vec3::normalize)
-        .set("flip", &vec3::flip)
-        .set("dot", &vec3::dot)
-        .set("cross", &vec3::cross)
-        .set("length", &vec3::length);
+       .constructor<float, float, float>()
+       .set("normalize", &vec3::normalize)
+       .set("flip", &vec3::flip)
+       .set("dot", &vec3::dot)
+       .set("cross", &vec3::cross)
+       .set("length", &vec3::length)
+	.set("x", &vec3::x)
+	.set("y", &vec3::y)
+	.set("z", &vec3::z);
 
 	Log->Print("Registered vec3 class");
 }
