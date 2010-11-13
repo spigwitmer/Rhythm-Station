782d7cdb89a2bdec5d97ce4535c614debeb46f4a
Colby Klein <shakesoda@gmail.com>
2010-11-12 16:57:03 -0800

    Now possible to get results from the database (returns map<string, string>). Add usage comment.

--- src/SQLiteDatabase.cpp
+++ src/SQLiteDatabase.cpp
@@ -1,18 +1,58 @@
 #include "SQLiteDatabase.h"
 #include "Logger.h"
+#include <sstream>
+#include <map>
 
 void SQLiteDatabase::Open(std::string db) {
-	if(sqlite3_open(db.c_str(), &db_handle) != SQLITE_OK)
+	// this can be written as simply if (sqlite3_open), but this is more clear.
+	if(sqlite3_open_v2(db.c_str(), &db_handle, NULL, NULL) != SQLITE_OK)
 		Log->Print("Error opening SQLite Database: \"" + db + "\"");
 }
 
 void SQLiteDatabase::Close() {
-
+	// close the database.
 	sqlite3_close(db_handle);
 }
 
 void SQLiteDatabase::Query(std::string sql_str) {
+	// reset query state.
 	sqlite3_finalize(last_query);
-	sqlite3_prepare(db_handle, sql_str.c_str(), sql_str.size(), &last_query, NULL);
+	sqlite3_reset(last_query);
+
+	// prepare the statement and run it
+	sqlite3_prepare_v2(db_handle, sql_str.c_str(), sql_str.size(), &last_query, NULL);
 	sqlite3_sql(last_query);
+}
+
+// returns false until it's done.
+bool SQLiteDatabase::Step() {
+	int status = sqlite3_step(last_query);
+	int cols = 0;
+	std::ostringstream err;
+
+	switch (status) {
+		case SQLITE_ERROR:
+			err << sqlite3_errmsg(db_handle);
+			Log->Print(err.str());
+		case SQLITE_DONE:
+			return false;
+		case SQLITE_ROW:
+			return true;
+		default:
+			break;
+	}
+
+	// you shouldn't end up here!
+	return false;
+}
+
+std::map<std::string, std::string> SQLiteDatabase::GetRow() {
+	std::map<std::string, std::string> results;
+	int cols = sqlite3_column_count(last_query);
+	for (int i = 0; i<cols; i++) {
+		// untested and likely broken!
+		std::string col_name = std::string(sqlite3_column_name(last_query, i));
+		results[col_name] = std::string((const char*)sqlite3_column_text(last_query, i));
+	}
+	return results;
 }
--- src/SQLiteDatabase.h
+++ src/SQLiteDatabase.h
@@ -3,12 +3,31 @@
 
 #include <sqlite3.h>
 #include <string>
+#include <map>
+
+/*
+ * Usage:
+ * // make database instance and create db in memory.
+ * SQLiteDatabase* db = new SQLiteDatabse();
+ * db->Open(":memory:");
+ * // run an sql statement and get results
+ * db->Query("SELECT * FROM foo");
+ * while(db->Step()) {
+ *	map<std::string, std::string> row = db->GetRow();
+ *	Log->Print(row["bar"]);
+ * }
+ * // finished
+ * db->Close();
+ */
 
 class SQLiteDatabase {
 public:
 	void Open(std::string db);
 	void Close();
 	void Query(std::string sql_str);
+	bool Step();
+	std::map<std::string, std::string> GetRow();
+
 private:
 	// Completely guessing at how to implement this. It's 95% likely to be wrong.
 	sqlite3* db_handle;
