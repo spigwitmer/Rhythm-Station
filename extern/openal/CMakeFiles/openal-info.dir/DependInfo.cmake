c7008b8b4faee99df3433fd24a1b8ddd654d73be
U-MAFIAWX\pat <pat@mafiawx.(none)>
2010-12-15 23:43:44 -0800

    HUUUEG amount of code changes and dependency management for win32

---
+++ extern/openal/CMakeFiles/openal-info.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/pat/rstation/Rhythm-Station/extern/openal/examples/openal-info.c" "/home/pat/rstation/Rhythm-Station/extern/openal/CMakeFiles/openal-info.dir/examples/openal-info.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "_WIN32"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pat/rstation/Rhythm-Station/extern/openal/CMakeFiles/OpenAL32.dir/DependInfo.cmake"
+  )
