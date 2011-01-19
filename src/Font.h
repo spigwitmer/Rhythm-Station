945344b29754525d09ab69bf200e9c9e393263e9
Colby Klein <shakesoda@gmail.com>
2011-01-19 14:27:56 -0800

    Font stubs.

--- src/Font.h
+++ src/Font.h
@@ -1,63 +1,22 @@
-#include <ft2build.h>
-#include FT_FREETYPE_H
+#ifndef _FONT_H_
+#define _FONT_H_
 
+#include "Object.h"
 #include <string>
-#include <vector>
-#include <stdexcept>
-#include <cstdarg>
-#include <sstream>
 
-class FTLibraryContainer
+/*
+ * A simple class for loading bitmap fonts (monospace, currently)
+ * TODO: Proportional width, ttf/otf support via freetype2.
+ */
+
+class Font : public Object
 {
 public:
-	FTLibraryContainer();
-	~FTLibraryContainer();
-
-	FT_Library& getLibrary();
-
+	void Load(std::string);
+	void Set(std::string);
+	
 private:
-	static FT_Library m_library;
+	std::string m_text;
 };
 
-class Font
-{
-public:
-	Font();
-	~Font();
-
-	void Load(const std::string& filename, unsigned int size);
-	void release();
-	
-	bool isValid() const;
-
-	void drawText(float x, float y, const char *str, ...) const;
-	void drawText(float x, float y, const std::string& str) const;
-	
-	std::ostream& beginDraw(float x, float y);
-	void endDraw();
-	
-	unsigned int calcStringWidth(const std::string& str) const;
-	unsigned int getHeight() const;
-
-private:
-	// leave copy constructor and operator= undefined to make noncopyable
-	Font(const Font&);
-	const Font& operator=(const Font&);
-	
-private:
-	// font data
-	unsigned int texID_;
-	unsigned int listBase_;
-	std::vector<unsigned char> widths_;
-	unsigned char height_;
-
-	// stream drawing stuff
-	std::ostringstream m_ss;
-	float m_X;
-	float m_Y;
-
-	static const unsigned int SPACE = 32;
-	static const unsigned int NUM_CHARS = 96;
-
-	static FTLibraryContainer m_library;
-};
+#endif
