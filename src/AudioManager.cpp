b17ec3cb7a3e5e71c7b06b77e0fba05b23f35cbe
Colby Klein <shakesoda@gmail.com>
2011-01-23 04:29:18 -0800

    Remove sine_wave test function.

--- src/AudioManager.cpp
+++ src/AudioManager.cpp
@@ -63,6 +63,7 @@ void AudioManager::Open()
 	// create device context
 	context = alcCreateContext(device, NULL);
 	alcMakeContextCurrent(context);
+	
 	// clear errors
 	alGetError();
 }
@@ -70,49 +71,9 @@ void AudioManager::Open()
 void AudioManager::Close()
 {
 	Clear();
+	
 	// clean up
 	alcMakeContextCurrent(NULL);
 	alcDestroyContext(context);
 	alcCloseDevice(device);
-}
-
-// sound test.
-int sine_wave()
-{
-	// create the buffers
-	SoundData *sound = new SoundData();
-	
-	if (alGetError() != AL_NO_ERROR)
-		return -1;
-		
-	ALsizei size, freq;
-	ALvoid *data;
-	// wave parameters and buffer size
-	unsigned char *sineWave;
-	int samples = 16;
-	int frequency = 250;
-	freq = samples * frequency;
-	size = 16384; // 16k buffer
-	// build the sine wave
-	sineWave = new unsigned char[size];
-	
-	for (int i = 0; i < size; i++)
-	{
-		float x = i * 360.f / (float)samples;
-		sineWave[i] = sinf(x * (3.14159 / 180))*128+128;
-	}
-	
-	data = sineWave;
-	delete[] sineWave;
-	alBufferData(sound->buffers[0], AL_FORMAT_STEREO16, data, size, freq);
-	
-	if (alGetError() != AL_NO_ERROR)
-		return -2;
-		
-	alSourcei(sound->source, AL_LOOPING, (ALboolean)true);
-	alSourcei(sound->source, AL_BUFFER, sound->buffers[0]);
-	alSourcef(sound->source, AL_GAIN, 0.5f); // volume
-	alSourcePlay(sound->source);
-	sound->Register();
-	return 0;
 }
