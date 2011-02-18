cec166bd6f6ec3e9e205f9467456661ca5fa3a28
Patrick McIlroy <itgpmc@gmail.com>
2011-02-18 01:26:44 -0800

    unit tests, we need more of them

---
+++ src/RSTestThreads.cpp
@@ -0,0 +1,24 @@
+#include "ThreadGroup.h"
+#include <iostream>
+
+using namespace std;
+
+void *SomeRandomFunctionThatAThreadCalls(void *data)
+{
+	cout << "HARRO FROM CHILD THREAD ^_^" << endl;
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	cout << "Rhythm-Station test suite: Threads" << endl;
+	RSThread thread;
+
+	thread.Start(SomeRandomFunctionThatAThreadCalls);
+
+	cout << "greetings hi hello this is parent thread yes hello" << endl;
+
+	thread.Wait();
+
+	return 0;
+}
