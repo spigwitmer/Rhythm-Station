2ef636eb46d561691f4d512a3e01123ab33cc0de
Colby Klein <shakesoda@gmail.com>
2011-03-11 03:11:53 -0800

    Some work on the NoteField class.

--- src/NoteField.cpp
+++ src/NoteField.cpp
@@ -1,11 +1,47 @@
-/*
- *  NoteField.cpp
- *  Rhythm-Station
- *
- *  Created by Colby Klein on 2/17/11.
- *  Copyright 2011 shakesoda!. All rights reserved.
- *
- */
-
 #include "NoteField.h"
 
+NoteField::NoteField()
+{
+	// TODO
+}
+
+NoteField::~NoteField()
+{
+	// TODO
+}
+
+void NoteField::setColumns(char cols)
+{
+	if (cols < 1)
+		cols = 4; // default
+	
+	mColumns = cols;
+}
+
+void NoteField::setNoteskin(std::string name)
+{
+	if (name.empty())
+		name = "default";
+
+	mNoteskin = name;
+}
+
+void NoteField::setSpeed(float multiplier)
+{
+	mSpeed = (multiplier > 0.0f) ? multiplier : 1.0f;
+}
+
+void NoteField::Load(std::string path)
+{
+	// TODO
+}
+	
+void NoteField::Update(double delta)
+{
+	// TODO
+}
+
+void NoteField::Draw()
+{
+	// TODO
+}
--- src/NoteField.h
+++ src/NoteField.h
@@ -1,4 +1,8 @@
+#ifndef _NOTE_FIELD_H_
+#define _NOTE_FIELD_H_
+
 #include "Object.h"
+#include "TimingData.h"
 
 class NoteField : public Object
 {
@@ -10,7 +14,26 @@ public:
 	
 	void Update(double delta);
 	void Draw();
+	
+	void setColumns(char cols);
+	void setNoteskin(std::string name);
+	void setSpeed(float multiplier);
+	
 private:
+	// Noteskin to load/store
+	std::string mNoteskin;
+	
+	// Number of columns
+	char mColumns;
+	
+	// Scroll rate multiplier
+	float mSpeed;
+	
 	// Song position
-	double position, offset;
+	double mPosition, mOffset;
+	
+	// Step/Chart data.
+	TimingData mChart;
 };
+
+#endif
