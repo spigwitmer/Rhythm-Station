2972f116dc2168aae840c51b204d8bcb8744708d
Colby Klein <shakesoda@gmail.com>
2011-03-21 03:39:27 -0700

    Add padding to images to handle repeating on edges showing, fix PNG alpha troubles.

--- Data/Shaders/generic.fs
+++ Data/Shaders/generic.fs
@@ -6,5 +6,10 @@ varying vec3 Normal;
 
 void main() {
 	vec4 texture = texture2D(Texture0, Coords);
+
+	// Fix white edges. Even if the image is pre-multiplied, this should be OK
+	texture *= texture.a;
+	
+	// Prevent optimizing out Normal!
 	gl_FragColor = texture * Color + vec4(Normal * 0.0, 0.0);
 }
