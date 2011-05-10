f4cb0967d6265d1431a69ba777dc72a0dea6723f
Colby Klein <shakesoda@gmail.com>
2011-05-10 13:59:30 -0700

    Type.h is only needed for dealing with int types on windows.

--- src/todo/Type.h
+++ src/todo/Type.h
@@ -1,10 +1,6 @@
 #ifndef _Type_H_
 #define _Type_H_
 
-#include <string>
-#include <math.h>
-#include <ostream>
-
 #ifndef _WIN32
 #include <stdint.h> // uint32 and such on Windows
 #else
@@ -15,18 +11,4 @@
 #define int64_t long long
 #endif
 
-#include <sstream>
-#include <glm/glm.hpp>
-
-typedef uint32_t u32;
-typedef int32_t i32;
-typedef uint64_t u64;
-typedef int64_t	i64;
-
-typedef glm::vec2 vec2;
-typedef glm::vec3 vec3;
-typedef glm::vec4 vec4;
-typedef glm::vec4 rgba;
-
 #endif
-
